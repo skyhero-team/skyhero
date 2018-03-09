b31f6fc6fcd0ee2bc1b210cb28c313493987737d
Guido García <guido.garciabernardo@telefonica.com>
2018-03-09 16:18:27 +0100

    feat: control Face API rate limit

--- drone-brain/index.js
+++ drone-brain/index.js
@@ -13,42 +13,55 @@ if (!process.env.DRONE_IP) {
 }
 
 const client  = drone.createClient({
-  ip: process.env.DRONE_IP || DRONE_IP_DEFAULT
+  ip: process.env.DRONE_IP || DRONE_IP_DEFAULT,
+  frameRate: 1,
+  imageSize: "640x360" // 480x270, 640x360 (default)
 });
 
 require('ar-drone-png-stream')(client, { port: 8000 }); // expose the stream to the public UI
 
 const pngStream = client.getPngStream();
 
+const IMAGE_PROCESSING_ENABLED = true;
+
+let imagesProcessed = 0;
 let analyzing = false; // we don't want to analyze more than 1 image at the same time
 
 // init image streaming
-pngStream.on('data', (buffer) => {
-  if (!analyzing) {
-    analyzing = true;
-    console.log('New image received from the dron: anzlyzing');
+if (IMAGE_PROCESSING_ENABLED) {
+  pngStream.on('data', (buffer) => {
+    // This is a protection to avoid the 20 req/sec imposed by Face API
+    imagesProcessed++;
+    if (imagesProcessed % 5 !== 0) {
+      return;
+    }
 
-    // XXX we could use the buffer without an intermediate local file (might be faster)
-    const fileName = './images/drone_' + Date.now() + '.png';
-    sharp(buffer)
-      .toFile(fileName)
-      .then(() => {
-          faces.findPeople(fileName, (err, faces) => {
-            if (err) {
-              console.error('Not able to find people', err);
-            } else {
-              console.log(faces);
-            }
+    if (!analyzing) {
+      analyzing = true;
+      console.log('New image received from the dron: anzlyzing');
 
+      // XXX we could use the buffer without an intermediate local file (might be faster)
+      const fileName = './images/drone_' + Date.now() + '.png';
+      sharp(buffer)
+        .toFile(fileName)
+        .then(() => {
+            faces.findPeople(fileName, (err, faces) => {
+              if (err) {
+                console.error('Not able to find people', err);
+              } else {
+                console.log(faces);
+              }
+
+              analyzing = false; // free the token to be able to process another picture
+            });
+        })
+        .catch(err => {
+            console.error('Something went wrong saving the image', err);
             analyzing = false; // free the token to be able to process another picture
-          });
-      })
-      .catch(err => {
-          console.error('Something went wrong saving the image', err);
-          analyzing = false; // free the token to be able to process another picture
-      });
-  };
-});
+        });
+    };
+  });
+}
 
 app.post('/', (req, res) => {
   console.log('Takeoff request received');
