559266dde06a803240e486352d2f4e1f2b3044a6
Guido García <guido.garciabernardo@telefonica.com>
2018-03-09 13:39:36 +0100

    feat: integrate webapp with drone control and basic face recognitzion

--- drone-brain/faces.js
+++ drone-brain/faces.js
@@ -1,46 +1,50 @@
 const cognitiveServices = require('cognitive-services');
 const fs = require('fs');
 
+if (!process.env.FACES_API_KEY) {
+  console.error('no FACES_API_KEY env var found');
+  process.exit(1);
+}
 const config = {
-    apiKey: process.env.FACES_API_KEY || 'xxx',
-    endpoint: 'westcentralus.api.cognitive.microsoft.com'
+  apiKey: process.env.FACES_API_KEY,
+  endpoint: 'westcentralus.api.cognitive.microsoft.com'
 };
 
 const face = new cognitiveServices.face(config);
 
 function findPeople(file, callback) {
-    fs.readFile(file, (err, body) => {
-        if (err) {
-            console.error('Not able to read image', err);
-            return callback(err);
-        }
+  fs.readFile(file, (err, body) => {
+    if (err) {
+      console.error('Not able to read image', err);
+      return callback(err);
+    }
 
-        // detect up to 64 faces
-        const parameters = {
-            returnFaceId: "true",
-            returnFaceLandmarks: "false"
-        };
+    // detect up to 64 faces
+    const parameters = {
+      returnFaceId: "true",
+      returnFaceLandmarks: "false"
+    };
 
-        const headers = {
-            'Content-type': 'application/octet-stream' // application/json + body with an url field to use an url
-        };
+    const headers = {
+      'Content-type': 'application/octet-stream' // application/json + body with an url field to use an url
+    };
 
-        face.detect({parameters, headers, body})
-            .then(faces => {
-                callback(null, faces);
-            })
-            .catch(err => callback(err));
-    });
+    face.detect({parameters, headers, body})
+    .then(faces => {
+      callback(null, faces);
+    })
+    .catch(err => callback(err));
+  });
 }
 
 /**
 // TODO use face lists to recognize people
 face.listFaceLists()
-    .then((lists) => {
-        console.log(lists);
-    }).catch((e) => console.error(e));
+.then((lists) => {
+  console.log(lists);
+}).catch((e) => console.error(e));
 */
 
 module.exports = {
-    find
+  findPeople
 }
--- drone-brain/index.js
+++ drone-brain/index.js
@@ -1,29 +1,67 @@
 const drone = require('ar-drone');
 const sharp = require('sharp');
+const faces = require('./faces');
 
-var client  = drone.createClient({
-  ip: '192.168.43.86'
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+const DRONE_IP_DEFAULT='192.168.43.86';
+if (!process.env.DRONE_IP) {
+  console.log('No DRONE_IP env var found. Using the default value %s', DRONE_IP_DEFAULT)
+}
+
+const client  = drone.createClient({
+  ip: process.env.DRONE_IP || DRONE_IP_DEFAULT
 });
 
-// client.land();
-client.animateLeds('blinkGreen', 5, 2)
+require('ar-drone-png-stream')(client, { port: 8000 }); // expose the stream to the public UI
 
-/**
-// image streaming
-var pngStream = client.getPngStream();
+const pngStream = client.getPngStream();
+
+// init image streaming
 pngStream.on('data', (buffer) => {
-    console.log('new image');
-    sharp(buffer).toFile('drone_' + Date.now() + '.png');
-});
-*/
+  console.log('new image received from the dron');
 
-/**
-client
-  .after(5000, function() {
-    this.stop();
+  // XXX we could use the buffer without an intermediate local file
+  const fileName = './imagen/drone_' + Date.now() + '.png';
+  sharp(buffer).toFile(fileName).then(() => {
+    faces.findPeople(fileName, (err, faces) => {
+      if (err) {
+        console.error('Not able to find people', err);
+        return;
+      }
+
+      console.log(faces);
+    });
   })
-  .after(5000, function() {
-    this.land();
-    console.log('END')
-  });
-*/
+});
+
+app.post('/', (req, res) => {
+  client.takeoff();
+  client.animateLeds('blinkGreen', 5, 2)
+
+  client
+    .after(5000, () => {
+      this.land();
+      console.log('Mission finished');
+    });
+
+  res.send('ok alexa');
+});
+
+app.listen(3000, () => {
+  console.log('Sky Hero app listening on port 3000');
+});
+
+process.on('SIGINT', function() {
+  console.log( "Gracefully shutting down from SIGINT (Ctrl-C)" );
+
+  // try to land the drone. we don't need an uncontrolled drone killing humans
+  if (client) {
+    client.land();
+  }
+
+  process.exit(0);
+});
